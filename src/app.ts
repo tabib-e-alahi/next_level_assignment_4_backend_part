7aa833a2b1f657a205bc067d554ed4daf0bc702d
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-05 05:54:18 +0600

    \`feat: Add express server and example env file\`

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Express } from "express";
+import cors from "cors";
+
+const app: Express = express();
+
+app.use(express.json);
+
+app.use(
+      cors({
+            origin: "*",
+            credentials: true
+      }),
+);
+
+export default app;
