60023a9d5ee2cc412232969a70795b01b4e88888
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-06 14:47:54 +0600

    Add bcrypt type definitions and hash password in auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,17 @@
 import { User } from "../../generated/prisma/client";
+import { prisma } from "../../lib/prisma";
+import bcrypt from "bcrypt";
 
-const registerUser = (payload: User) =>{
-      
+const registerUser = async (payload: User) => {
+      const hashedPass = await bcrypt.hash(payload.password, 10);
+      const result = await prisma.user.create({
+            data: {
+                  ...payload,
+                  password: hashedPass
+            }
+      })
+
+      return result;
 }
 
 export const authService = {
