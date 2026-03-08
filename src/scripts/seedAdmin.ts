8db9db7a6d4339cac81b431223d27b751241e0ce
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-09 01:07:01 +0600

    Add admin seeding script

---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,41 @@
+
+import bcrypt from "bcryptjs";
+import { UserRole } from "../middlewares/auth";
+import { prisma } from "../lib/prisma";
+
+const seedAdmin = async () => {
+  const hashedPassword = await bcrypt.hash(process.env.ADMIN_PASSWORD as string, 10);
+
+  const adminData = {
+    name: process.env.ADMIN_NAME!,
+    email: process.env.ADMIN_EMAIL!,
+    role: UserRole.ADMIN,
+    password: hashedPassword,
+  };
+
+  // 2.if Exists then return
+  // 3.If not then create
+
+  try {
+    const isAdminAlreadyExists = await prisma.user.findUnique({
+      where: {
+        email: adminData.email as string,
+      },
+    });
+
+    if (isAdminAlreadyExists) {
+      console.log("Admin already exists!!");
+      return;
+    }
+    const admin = await prisma.user.create({
+      data: adminData,
+    });
+
+    console.log("Admin created successfully!!", admin);
+  } catch (error) {
+    console.log(error);
+  } finally{
+    await prisma.$disconnect()
+  }
+};
+seedAdmin();
