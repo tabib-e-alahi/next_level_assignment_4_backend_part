ac5922ecd1a7737e4edab0db216cceada1a6abb8
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-09 19:37:44 +0600

    Add filters to meals API endpoint

--- src/modules/meals/meals.service.ts
+++ src/modules/meals/meals.service.ts
@@ -1,6 +1,21 @@
 import { prisma } from "../../lib/prisma"
 
-const getAllMeals = async () => {
+const getAllMeals = async ({
+      search,
+      cuisine,
+      dietaryTags,
+      minPrice,
+      maxPrice
+}: {
+      search: string | undefined
+      cuisine: string | undefined
+      dietaryTags: string[] | []
+      minPrice: string | undefined
+      maxPrice: string | undefined
+}) => {
+      console.log(search, cuisine, dietaryTags,
+            minPrice,
+            maxPrice);
       const result = await prisma.meals.findMany();
 
       return result;
