426bfe1b72d732b6c47d1eb2be03f6103e2e6f86
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-12 16:28:25 +0600

    Update customer controller and service for profile fetching

--- src/modules/customer/customer.controller.ts
+++ src/modules/customer/customer.controller.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
 import sendError from '../../utils/sendError';
 import { customerService } from './customer.service';
-const getProfile = async(req: Request, res: Response) =>{
+import sendResponse from '../../utils/sendResponse';
+const getProfile = async (req: Request, res: Response) => {
       try {
-            const userId = req.user.id; 
-            const result  = await customerService.getProfile(userId)
+            const userId = req.user.id;
+            const result = await customerService.getProfile(userId);
+
+            return sendResponse(res, 200, "Customer data fetched successfully", result)
       } catch (error) {
             return sendError(res, 500, "Could not get profile data", error)
       }
--- src/modules/customer/customer.service.ts
+++ src/modules/customer/customer.service.ts
@@ -1,15 +1,28 @@
 import { prisma } from "../../lib/prisma";
 
-const getProfile = async(userId: string | undefined) =>{
-      if(!userId){
+const getProfile = async (userId: string | undefined) => {
+      if (!userId) {
             throw new Error("Invalid userid");
       }
 
-      const result  =await prisma.user.findUniqueOrThrow({
-            where:{
+      const result = await prisma.user.findUnique({
+            where: {
                   id: userId
-            }
+            },
+            select: {
+                  id: true,
+                  name: true,
+                  email: true,
+                  phone: true,
+                  status: true,
+                  createdAt: true,
+                  updatedAt: true,
+            },
       })
+
+      if(!result){
+            throw new Error("Customer data not found");
+      }
 
       return result;
 }
