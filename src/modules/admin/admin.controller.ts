74967ff7a21a95138d3229293f29aee7ad8b93d9
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-12 08:48:42 +0600

    Add admin role check to getAllUsers endpoint

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -1,9 +1,20 @@
 import { Request, Response } from 'express';
 import sendError from '../../utils/sendError';
-const getAllUsers = async(req:Request, res: Response) =>{
+import { UserRole } from '../../middlewares/auth';
+import { prisma } from '../../lib/prisma';
+const getAllUsers = async (req: Request, res: Response) => {
       try {
             const user = req.user;
-            console.log(user.role);  
+            if (user.role !== UserRole.ADMIN) {
+                  return sendError(res, 403, "Forbidden Access!! Only for Admin.")
+            }
+
+            const users = await prisma.user.findMany({
+                  include: {
+                        providerProfiles: true, 
+                  },
+            });
+            
       } catch (error) {
             return sendError(res, 500, "Could not fetched users data", error)
       }
