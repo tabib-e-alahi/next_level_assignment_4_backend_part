18f37ba39414f31299fe1201911752b60b57dc56
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-12 08:42:42 +0600

    Add admin route

--- src/modules/admin/admin.routes.ts
+++ src/modules/admin/admin.routes.ts
@@ -3,6 +3,9 @@ import { adminController } from './admin.controller';
 import auth, { UserRole } from '../../middlewares/auth';
 
 const router: Router = express.Router();
+router.get("/", (req, res) => {
+  res.send("Admin route working");
+});
 
 router.get("/users", auth(UserRole.ADMIN), adminController.getAllUsers);
 
