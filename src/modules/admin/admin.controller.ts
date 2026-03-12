9818161cdc6a92d305bd676d2458ed370a3317cf
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-12 08:28:42 +0600

    Update admin controller to handle user data fetching errors

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -2,9 +2,9 @@ import { Request, Response } from 'express';
 import sendError from '../../utils/sendError';
 const getAllUsers = async(req:Request, res: Response) =>{
       try {
-            
+             
       } catch (error) {
-            return sendError
+            return sendError(res, 500, "Could not fetched users data", error)
       }
 }
 
