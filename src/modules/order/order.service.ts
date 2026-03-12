56d083d1eea7c2433e44eda111fec03fe5268354
tabib-e-alahi <etabib.alahi@gmail.com>
2026-03-12 07:00:42 +0600

    \`Refactor order creation logic in order service\`

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -24,117 +24,106 @@ const createOrder = async (userId: string, deliveryAddress: string) => {
       }
 
       const cartItems = cartData.items;
-      const createdOrders = [];
 
-      const totalAmount = cartItems.reduce((total, item) => {
-            return total + item.quantity * item.meal.price;
-      }, 0);
+      const createdOrders = [];
+//! -----------------
       for (const item of cartItems) {
             const providerId = item.meal.providerId;
-            const totalAmount = cartItems.filter(i => i.meal.providerId === providerId)
-                  .reduce((total, i) => total + i.quantity * i.meal.price, 0);
+            const totalAmount = cartItems.filter(itm => itm.meal.providerId === providerId)
+                  .reduce((total, itm) => total + itm.quantity * itm.meal.price, 0);
 
-      const jkf = {
-                  id: 'e769fa2f-859a-4588-aa09-b56dd0e92341',
-                  customerId: 'bbdb6500-9a9b-40aa-be4a-a204c529ad78',
-                  items: [
-                        {
-                              id: '4de4b2e2-523b-4fd8-a705-0533932e632b',
-                              cartId: 'e769fa2f-859a-4588-aa09-b56dd0e92341',
-                              mealId: '81297cda-6c5e-464b-9824-98f9e1bd0b5e',
-                              quantity: 45,
-                              meal: [Object]
-                        },
-                        {
-                              id: '28eed6f6-8d37-4fd0-81ba-c60d94ba36c5',
-                              cartId: 'e769fa2f-859a-4588-aa09-b56dd0e92341',
-                              mealId: 'f0e122a6-1cda-4e4d-b36b-a8a2f1750fe5',
-                              quantity: 30,
-                              meal: [Object]
-                        }
-                  ]
-            }
+            // Step 3: Create a new order for this provider
+            const newOrder = await prisma.order.create({
+                  data: {
+                        customerId: userId,
+                        deliveryAddress,
+                        totalAmount,
+                        providerId,
+                  },
+            });
 
-            const result = await prisma.$transaction(async (tx) => {
-                  let totalOrderData = []
-                  for (const item of cartItems) {
-                        const newOrder = await tx.order.create({
-                              data: {
-                                    customerId: userId,
-                                    deliveryAddress,
-                                    totalAmount,
-                                    providerId: cartItems[0].meal.providerId,
-                              },
-                        });
-                        await tx.orderItem.create({
-                              data: {
-                                    orderId: newOrder.id,
-                                    mealId: item.mealId,
-                                    quantity: item.quantity,
-                                    unitPrice: item.meal.price,
-                              },
-                        });
-                  }
-                  // now cart theke item gulo delete 
-                  await tx.cartItem.deleteMany({
-                        where: {
-                              cartId: cartData.id,
+      }
+//! ---------------
+      const result = await prisma.$transaction(async (tx) => {
+            let totalOrderData = []
+            for (const item of cartItems) {
+                  const newOrder = await tx.order.create({
+                        data: {
+                              customerId: userId,
+                              deliveryAddress,
+                              totalAmount,
+                              providerId: cartItems[0].meal.providerId,
                         },
                   });
-
-                  return newOrder;
-            });
-
-            return result;
-      }
-
-
-      const getMyOrders = async (userId: string) => {
-            const result = await prisma.order.findMany({
-                  where: {
-                        customerId: userId,
-                  },
-                  include: {
-                        orderItems: {
-                              include: {
-                                    meal: true,
-                              },
+                  await tx.orderItem.create({
+                        data: {
+                              orderId: newOrder.id,
+                              mealId: item.mealId,
+                              quantity: item.quantity,
+                              unitPrice: item.meal.price,
                         },
-                  },
-                  orderBy: {
-                        createdAt: "desc",
-                  },
-            });
-
-            return result;
-      }
-      const getMyOrderById = async (userId: string, orderId: string) => {
-            const result = await prisma.order.findFirst({
-                  where: {
-                        id: orderId,
-                        customerId: userId,
-                  },
-                  include: {
-                        orderItems: {
-                              include: {
-                                    meal: true,
-                              },
-                        },
-                  },
-            });
-
-            if (!result) {
-                  throw new Error("Order not found")
+                  });
             }
+            // now cart theke item gulo delete 
+            await tx.cartItem.deleteMany({
+                  where: {
+                        cartId: cartData.id,
+                  },
+            });
 
-            return result;
+            return newOrder;
+      });
+
+      return result;
+}
+
+
+const getMyOrders = async (userId: string) => {
+      const result = await prisma.order.findMany({
+            where: {
+                  customerId: userId,
+            },
+            include: {
+                  orderItems: {
+                        include: {
+                              meal: true,
+                        },
+                  },
+            },
+            orderBy: {
+                  createdAt: "desc",
+            },
+      });
+
+      return result;
+}
+const getMyOrderById = async (userId: string, orderId: string) => {
+      const result = await prisma.order.findFirst({
+            where: {
+                  id: orderId,
+                  customerId: userId,
+            },
+            include: {
+                  orderItems: {
+                        include: {
+                              meal: true,
+                        },
+                  },
+            },
+      });
+
+      if (!result) {
+            throw new Error("Order not found")
       }
 
-      export const orderService = {
-            createOrder,
-            getMyOrders,
-            getMyOrderById
-      }
+      return result;
+}
+
+export const orderService = {
+      createOrder,
+      getMyOrders,
+      getMyOrderById
+}
 
 // [
 //   {
